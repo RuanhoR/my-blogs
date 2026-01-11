ec383e1c9202fda3babc6f8a64616ddcbd040404
Ruanhor <3915264929@qq.com>
2026-01-11 14:32:09 +0000

    fixed md title not show

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,22 +1,18 @@
-import {
-  h
-} from 'vue'
-import type {
-  Theme
-} from 'vitepress'
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import UmamiConfig from "./umami.config.js"
-const Layout = () => {
-  return h("div", null, [
-    h(DefaultTheme.Layout, null, {}),
-    h("script", {
-      "defer": "defer",
-      "src": "https://cloud.umami.is/script.js",
-      "data-website-id": UmamiConfig.token
-    }, {})
+import UmamiConfig from './umami.config.js'
+// 自定义 Layout 组件
+const Layout = (props, { slots }) => {
+  return h('div', [
+    h(DefaultTheme.Layout, props, slots),
+    h('script', {
+      defer: true,
+      src: 'https://cloud.umami.is/script.js',
+      'data-website-id': UmamiConfig.token,
+    })
   ])
 }
 export default {
   extends: DefaultTheme,
-  Layout,
+  Layout
 }
