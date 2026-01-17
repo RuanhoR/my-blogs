055b5ebd854853c7afd3680b02dc227aedfb585c
Ruanhor <3915264929@qq.com>
2026-01-17 12:27:28 +0000

    docs

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 import UmamiConfig from './umami.config.js'
 // 自定义 Layout 组件
 const Layout = (props, { slots }) => {
+  console.log(slots)
   return h('div', [
     h(DefaultTheme.Layout, props, slots),
     h('script', {
